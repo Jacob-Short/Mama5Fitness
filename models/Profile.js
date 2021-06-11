f17fdb65598bcca94cdde87dede42bfcbe3e652b
Jacob Short <thatmanjshort@gmail.com>
2021-06-11 12:56:45 -0700

    added socials to profile schema

--- models/Profile.js
+++ models/Profile.js
@@ -2,28 +2,44 @@ const mongoose = require("mongoose");
 
 // Build profile schema
 const ProfileSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-  },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+    },
 
-  bio: {
-    type: String,
-  },
-  favoritemovie: {
-    type: String,
-  },
-  favoritegame: {
-    type: String,
-  },
-  favoritetvseries: {
-    type: String,
-  },
-
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+    bio: {
+        type: String,
+    },
+    favoritemovie: {
+        type: String,
+    },
+    favoritegame: {
+        type: String,
+    },
+    favoritetvseries: {
+        type: String,
+    },
+    social: {
+    youtube: {
+            type: String
+        },
+    twitter: {
+            type: String
+        },
+    facebook: {
+            type: String
+        },
+    linkedin: {
+        type: String
+        },
+    instagram: {
+        type: String
+        } 
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    }
 });
 
 module.exports = mongoose.model("profile", ProfileSchema);
